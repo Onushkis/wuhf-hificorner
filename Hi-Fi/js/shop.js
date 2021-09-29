7c04caec1979c9e98a4ac06c904e577130bc348c
Ona <onushkis@gmail.com>
2021-09-29 22:35:51 +0200

    add new gallery Nav categories

--- Hi-Fi/js/shop.js
+++ Hi-Fi/js/shop.js
@@ -1,187 +1,69 @@
 document.addEventListener("DOMContentLoaded", () => {
-
-
   // Create the main category gallery
   fetchCategory();
 
   function fetchCategory() {
-
     let shopGallery = document.querySelector(".CategoryImg");
 
     fetch("./js/shop.json")
       .then((response) => response.json())
       .then((data) => {
-
         console.log(data);
 
         /*everything is showing up in html*/
 
-        var categoryName = getCategory();
+        var categoryNameFromHTML = getCategory();
 
-        if (categoryName === "Amplifiers") {
+        data.products.forEach((product) => {
+          if (product.category_name === categoryNameFromHTML) {
+            var buttonString = "";
+            if (product.hasButom === true) {
+              //if btnString has a value of true it shows in the view
+              buttonString = `<button class="CategoryImg__gallery__add">Add to cart</button> `;
+            }
 
-          data.Amplifiers.forEach((category) => {
-
-            if (category.hasButom === true) {
-
-              shopGallery.innerHTML += `
+            shopGallery.innerHTML += `
   
    
-            <div class="CategoryImg__gallery">
-            
-    
-            <div class="CategoryImg__img">
-                <img src="images/cd_afspillere/${category.image}" alt="">
-            
-                <p class="CategoryImg__text">yamaha R-N402 network <br> HiFI Receiver</p>
-                <p class="CategoryImg__price">£429.00 £329.00</p>
-            
-            <div class="CategoryImg__btn">
-                <button class="CategoryImg__gallery__add">Add to cart</button>
-            </div>
-            </div>
-            </div>    
-            `;
-
-
-            }
-            else {
-              shopGallery.innerHTML += `
-  
-   
-  <div class="CategoryImg__gallery">  
-  
-  <div class="CategoryImg__img">
-    <img src="images/cd_afspillere/${category.image}" alt="">
-  
-    <p class="CategoryImg__text">yamaha R-N402 network <br> HiFI Receiver</p>
-    <p class="CategoryImg__price">£429.00 £329.00</p>  
-  </div> 
-  </div>  
-  `;
-            }
-          })
-        }
-        else if (categoryName === "Speakers") {
-
-          data.Speakers.forEach((category) => {
-
-            if (category.hasButom === true) {
-              shopGallery.innerHTML += `
-  
-   
-            <div class="CategoryImg__gallery">
-            
-    
-            <div class="CategoryImg__img">
-                <img src="images/hojtalere/${category.image}" alt="">
-            
-                <p class="CategoryImg__text">yamaha R-N402 network <br> HiFI Receiver</p>
-                <p class="CategoryImg__price">£429.00 £329.00</p>
-            
-            <div class="CategoryImg__btn">
-                <button class="CategoryImg__gallery__add">Add to cart</button>
-            </div>
-            </div>
-            </div>    
-            `;
-
-
-            }
-            else {
-              shopGallery.innerHTML += `
-  
-   
-  <div class="CategoryImg__gallery">  
-  
-  <div class="CategoryImg__img">
-    <img src="images/hojtalere/${category.image}" alt="">
-  
-    <p class="CategoryImg__text">yamaha R-N402 network <br> HiFI Receiver</p>
-    <p class="CategoryImg__price">£429.00 £329.00</p>  
-
-
-  </div> 
-  </div>  
-  `;
-            }
-          })
-        }
-
-        else if (categoryName === "TurnTables") {
-
-          data.TurnTables.forEach((category) => {
-
-            if (category.hasButom === true) {
-              shopGallery.innerHTML += `
-  
-   
-            <div class="CategoryImg__gallery">
-            
-    
-            <div class="CategoryImg__img">
-                <img src="images/forforstaerkere/${category.image}" alt="">
-            
-                <p class="CategoryImg__text">yamaha R-N402 network <br> HiFI Receiver</p>
-                <p class="CategoryImg__price">£429.00 £329.00</p>
-            
-            <div class="CategoryImg__btn">
-                <button class="CategoryImg__gallery__add">Add to cart</button>
-            </div>
-            </div>
-            </div>    
-            `;
-
-
-            }
-            else {
-              shopGallery.innerHTML += `
-  
-   
-  <div class="CategoryImg__gallery">  
-  
-  <div class="CategoryImg__img">
-    <img src="images/forforstaerkere/${category.image}" alt="">
-  
-    <p class="CategoryImg__text">yamaha R-N402 network <br> HiFI Receiver</p>
-    <p class="CategoryImg__price">£429.00 £329.00</p>  
-
-
-  </div> 
-  </div>  
-  `;
-            }
-          })
-        }
-      })
-  }
-
-
+   <div class="CategoryImg__gallery">
   
 
-  function getCategory() {
+ <div class="CategoryImg__img">
+     <img src="images/cd_afspillere/${product.image}" alt="">
+  
+       <p class="CategoryImg__text">yamaha R-N402 network <br> HiFI Receiver</p>
+       <p class="CategoryImg__price">£429.00 £329.00</p>
+  
+   <div class="CategoryImg__btn">
+   ${buttonString}
+   </div>
+  </div>
+   </div>    
+   `;
+          }
+        });
+      });
 
-    var url_string = window.location.href
-    var url = new URL(url_string);
-    var category_name = url.searchParams.get("categoryName");
-    return category_name
-  }
+    function getCategory() {
+      var url_string = window.location.href;
+      var url = new URL(url_string);
+      var category_name = url.searchParams.get("categoryName");
+      return category_name;
+    }
 
+    // Create the Div Left
 
-  // Create the Div Left
+    fetch("./js/divLeft.json")
+      .then((response) => response.json())
+      .then((data) => {
+        data.Categories.forEach((category) => {
+          let divLeftCategory = document.querySelector(
+            ".Main_shop_category__ul"
+          );
 
+          console.log(category.label);
 
-  fetch("./js/divLeft.json")
-    .then((response) => response.json())
-    .then((data) => {
-
-      data.Categories.forEach((category) => {
-
-        let divLeftCategory = document.querySelector(".Main_shop_category__ul");
-
-        console.log(category.label);
-
-        divLeftCategory.innerHTML += `
+          divLeftCategory.innerHTML += `
        
      
     
@@ -190,38 +72,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
          
       `;
-      })
-
-    });
-
-
-  
-
-
+        });
+      });
+  }
 });
-
-          // --------her finde jeg alle Arrays og skriver i innerHtml det hele
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
